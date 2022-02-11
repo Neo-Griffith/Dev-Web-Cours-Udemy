d951f91e0c3e4e7dbf006bc9e9a65215bc65a325
Neo-Griffith <37379556+Neo-Griffith@users.noreply.github.com>
2022-02-11 17:31:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+var xString ="12.63"; 
+var xNumber = parseFloat(xString);
+console.log(xNumber);
+
+var myString = "Hello Coco !!!";
+var myNewString = myString.replace("Coco","Malo");
+console.log(myNewString);
+
+var string1 = "Hello";
+var string2 = 'Coco';
+var myNewString2 = string1+" "+string2;
+console.log(myNewString2);
+
+var x = 34;
+x-=4 ;
+
+
+console.log(x);
+
+var string3 =  "hello";
+var string4 = "Corentin";
+//Affiche l'addition de string 3 et 4
+console.log(string3+" "+string4);
+/* Bloc entier de commentaire 
+
+Bloc entier de commentaire */
+
+var x = 5;
+var y = 12;
+var myBoolean= (x>10 || y<15);
+console.log(myBoolean);
+console.log(!true);
+console.log(!false);
+
+
+if (condition )
+{
+    console.log("Execute ce code !!!!"); 
+}
+else{
+    console.log("Execute ce code la !!!!");
+}
